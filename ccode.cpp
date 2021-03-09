a47c1f031b6b9c104b976abbfdc1a7b13ef64b80
vineethraik <vineethr.ec18@sahyadri.edu.in>
2021-03-10 00:21:25 +0530

    added test cpp file

--- ccode.cpp
+++ ccode.cpp
@@ -1,14 +1,60 @@
 /*c code*/
 
-#include"network.h"
+#include"neuralnetwork.h"
+#include<windows.h>
 
 
 
 int main()
 {
-    inputset x,y;
+  srand(time(0));
+    dataset x,y;
     readfile(x,"input.txt");
     x.read();
-    return 0;
+    
+
+   /* 
+  layer a(5,5);
+  layer b(7,4);
+  a.readout();
+  b.readout();
+ 
+    
+   /* for(int i=0;i<10;i++){
+    a.init(5);
+    a.readwt();
+    }
+    neuron a(9);
+    a.readwt();
+    neuron b(15);
+    b.readwt();
+      
+vector<double> wt;
+
+for(int i=0;i<=10;i++){
+  //wt.push_back((-9999+(rand()%19999))/1000.0);
+  wt.push_back(i);
+  
+  
+}
+for(int i=0;i<wt.size();i++){
+cout<<wt[i]<<" ";
+}
+cout<<"\n";
+wt.clear();
+for(int i=0;i<=15;i++){
+  //wt.push_back((-9999+(rand()%19999))/1000.0);
+  wt.push_back(i);
+  
+}
+for(int i=0;i<wt.size();i++){
+cout<<wt[i]<<" ";
+}
+*/
+
+
+    
+   
+return 0;
 }
 
