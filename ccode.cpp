1f7bdaedf59cd2f9b27db0b861c367147cbd847f
vineethraik <vineethraik23082000@gmail.com>
2021-04-02 20:39:24 +0530

    safe keeping

--- ccode.cpp
+++ ccode.cpp
@@ -15,12 +15,18 @@ int main()
   network a;
   vector<int> structure;          //variable which is used to define the structure of nn 
   structure.push_back(x.ilen);    //input neurons
-  structure.push_back(2);         //2nd layer neurons
+  structure.push_back(5);         //2nd layer neurons
   structure.push_back(x.olen);    //output neurons
   a.init(x,structure);            //initializing neuron using dataset and structure
   a.readout();
   temp=a.feedforword();           
   x.compare(temp);
+  a.backprop(5000);
+  cout<<"\n";
+  temp=a.feedforword();           
+  x.compare(temp);
+  cout<<"\n";
+  //a.readout();
     
   
   return 0;
