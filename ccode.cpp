8ae8fbfd47f85e6f386c626df70b88bb5d8c4399
vineethraik <vineethraik23082000@gmail.com>
2021-07-12 07:20:26 +0530

    cleen slate

--- ccode.cpp
+++ ccode.cpp
@@ -12,23 +12,7 @@ int main()
 
   readfile(x,"t.txt");        //gathering training data from input.txt file
   //x.read();                       
-  network a;
-  vector<int> structure;          //variable which is used to define the structure of nn 
-  structure.push_back(x.ilen);    //input neurons
-  structure.push_back(1);         //2nd layer neurons
-  structure.push_back(1);         //2nd layer neurons
-  structure.push_back(1);         //2nd layer neurons
-  structure.push_back(x.olen);    //output neurons
-  a.init(x,structure);            //initializing neuron using dataset and structure
-  a.readout();
-  temp=a.feedforword();           
-  x.compare(temp);
-  a.backprop(50000);
-  cout<<"\n";
-  temp=a.feedforword();           
-  x.compare(temp);
-  cout<<"\n";
-  a.readout();
+  
     
   
   return 0;
